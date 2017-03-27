47d9e19473ae526372b88684b3c976b6c9c83243
OthoM <otho.mantegazza@gmail.com>
2017-03-27 17:19:16 +0200

    add formula in dat_fit param

--- R/linear_mod.R
+++ R/linear_mod.R
@@ -1,19 +1,22 @@
 #' Add F-value from Linear Model to Orthologroup Expression Set
 
-add_fit <- function(dat, coldata)
+add_fit <- function(dat, coldata, formula)
 {
+    vars <- all.vars(formula)
+    vars <- vars[!vars == "exp"]
+    stopifnot(all(vars %in% colnames(coldata)))
     fit_anova_int <- function(ogroup, dset, coldata)
     {
         dat_temp <- t(dset)
         to_fit <- cbind(exp = dat_temp[, ogroup], coldata)
-        fit <- lm(exp ~ 0 + spc + stg + spc:stg, data = to_fit)
+        fit <- lm(formula = formula, data = to_fit)
         anova_of_fit <- anova(fit)
         out <- anova_of_fit$`F value`
         names(out) <- attributes(anova_of_fit)$row.names
         return(out)
     }
 
-    fits <- mclapply(rownames(dat),
+    fits <- parallel::mclapply(rownames(dat),
                      fit_anova_int, dset = dat, coldata = coldata,
                      mc.cores = 4)
 
@@ -23,4 +26,15 @@ add_fit <- function(dat, coldata)
     dat_fit <- merge(dat, fits, by = "row.names"); rownames(dat_fit) <- dat_fit$Row.names; dat_fit$Row.names <- NULL
     dat_fit <- dat_fit[order(dat_fit$`spc:stg`, decreasing = T), ]
     return(dat_fit)
+}
+
+if(FALSE) {
+load("tests/testthat/sample_oges.Rdata")
+require(devtools)
+load_all()
+coldata <- data.frame(spc = substr(colnames(dat), 1, 2),
+                      stg = substr(colnames(dat), 4, 4),
+                      row.names = colnames(dat))
+model.matrix(coldata, 0 + spc + stg + spc:stg)
+add_fit(dat = dat, coldata = coldata, formula = exp ~ 0 + spc + stg + spc:stg)
 }
--- tests/testthat/test-lm.R
+++ tests/testthat/test-lm.R
@@ -1,5 +1,15 @@
+require(testthat)
+
 context("linear model and ANOVA work correctly")
 
-testthat("add_fit takes any coldata"{
+test_that("add_fit takes any coldata", {
+    load(file = "sample_oges.Rdata")
+    coldata <- data.frame(spc = substr(colnames(dat), 1, 2),
+                          stg = substr(colnames(dat), 4, 4),
+                          row.names = colnames(dat))
 
+    expect_error(add_fit(dat = dat, coldata = coldata, formula = ciao))
+    expect_s3_class(add_fit(dat = dat,
+                            coldata = coldata,
+                            formula = exp ~ 0 + spc + stg + spc:stg), "data.frame")
 })
