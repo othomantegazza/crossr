ce2319fdf8abd9260d41a4e2c14abac420b72e80
OthoM <otho.mantegazza@gmail.com>
2017-03-28 19:54:20 +0200

    improve formula in dat_fit

--- R/linear_mod.R
+++ R/linear_mod.R
@@ -1,15 +1,20 @@
-#' Add F-value from Linear Model to Orthologroup Expression Set
+#' Add F-values From Linear Model to Orthologroup Expression Set
+#'
+#' @param dat \code{data.frame} contains data for any gene
+#' @param coldata \code{data.frame} categorical variables with info for every samples
+#' @param design \code{formula}, encodes the assumption on how the variables in coldata
+#' explain the observed gene expression
 
-add_fit <- function(dat, coldata, formula)
+add_fit <- function(dat, coldata, design)
 {
-    vars <- all.vars(formula)
-    vars <- vars[!vars == "exp"]
+    vars <- all.vars(design)
     stopifnot(all(vars %in% colnames(coldata)))
+    design <- as.formula(paste("exp", deparse(design)))
     fit_anova_int <- function(ogroup, dset, coldata)
     {
         dat_temp <- t(dset)
         to_fit <- cbind(exp = dat_temp[, ogroup], coldata)
-        fit <- lm(formula = formula, data = to_fit)
+        fit <- lm(formula = design, data = to_fit)
         anova_of_fit <- anova(fit)
         out <- anova_of_fit$`F value`
         names(out) <- attributes(anova_of_fit)$row.names
@@ -29,12 +34,18 @@ add_fit <- function(dat, coldata, formula)
 }
 
 if(FALSE) {
-load("tests/testthat/sample_oges.Rdata")
-require(devtools)
-load_all()
-coldata <- data.frame(spc = substr(colnames(dat), 1, 2),
-                      stg = substr(colnames(dat), 4, 4),
-                      row.names = colnames(dat))
-model.matrix(coldata, 0 + spc + stg + spc:stg)
-add_fit(dat = dat, coldata = coldata, formula = exp ~ 0 + spc + stg + spc:stg)
+    load("tests/testthat/sample_oges.Rdata")
+    require(devtools)
+    load_all()
+    coldata <- data.frame(spc = substr(colnames(dat), 1, 2),
+                          stg = substr(colnames(dat), 4, 4),
+                          row.names = colnames(dat))
+    tst1 <- add_fit(dat = dat, coldata = coldata, design = exp ~ 0 + spc + stg + spc:stg)
+    tst2 <- add_fit(dat = dat, coldata = coldata, design = . ~ 0 + spc + stg + spc:stg)
+    tst3 <- add_fit(dat = dat, coldata = coldata, design = ~ 0 + spc + stg + spc:stg)
+
+
+    dt_tst <- cbind(exp = dat[1, ], coldata)
+    fit1 <- lm(exp ~ 0 + spc + stg + spc:stg, data = dt_tst)
+    fit2 <- lm( ~ 0 + spc + stg + spc:stg, data = dt_tst)
 }
--- tests/testthat/test-lm.R
+++ tests/testthat/test-lm.R
@@ -2,14 +2,14 @@ require(testthat)
 
 context("linear model and ANOVA work correctly")
 
-test_that("add_fit takes any coldata", {
+test_that("add_fit takes any design within coldata", {
     load(file = "sample_oges.Rdata")
     coldata <- data.frame(spc = substr(colnames(dat), 1, 2),
                           stg = substr(colnames(dat), 4, 4),
                           row.names = colnames(dat))
 
-    expect_error(add_fit(dat = dat, coldata = coldata, formula = ciao))
+    expect_error(add_fit(dat = dat, coldata = coldata, design = ciao))
     expect_s3_class(add_fit(dat = dat,
                             coldata = coldata,
-                            formula = exp ~ 0 + spc + stg + spc:stg), "data.frame")
+                            design = ~ 0 + spc + stg + spc:stg), "data.frame")
 })
